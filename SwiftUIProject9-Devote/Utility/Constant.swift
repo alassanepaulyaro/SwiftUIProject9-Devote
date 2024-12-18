fd13d6637080a94b59db7abc3682b1aef88f8974
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-12-18 10:29:19 +0100

    adding core data

---
+++ SwiftUIProject9-Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  SwiftUIProject9-Devote
+//
+//  Created by Yaro Paul  on 18/12/2024.
+//
+
+import Foundation
+
+// date formater
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// UX
+
+
