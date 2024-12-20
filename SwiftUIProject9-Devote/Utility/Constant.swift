186d6c56f19dc6328b52da31aebe171ced9bbf8c
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-12-20 11:03:38 +0100

    adding feedback

--- SwiftUIProject9-Devote/Utility/Constant.swift
+++ SwiftUIProject9-Devote/Utility/Constant.swift
@@ -20,3 +20,5 @@ let itemFormatter: DateFormatter = {
 var backgroundGradient: LinearGradient {
   return LinearGradient(gradient: Gradient(colors: [Color.pink, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
 }
+
+let feedback = UINotificationFeedbackGenerator()
